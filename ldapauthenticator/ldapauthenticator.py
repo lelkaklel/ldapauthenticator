6c3982ca7a965fd85ceb2fe9c5497fafa94678a0
Aleksey Soloviov <lelkaklel@gmail.com>
2017-07-04 17:34:16 +0300

    added LDAPLocalAuthenticator

--- ldapauthenticator/ldapauthenticator.py
+++ ldapauthenticator/ldapauthenticator.py
@@ -2,7 +2,7 @@ import ldap3
 from ldap3.utils.conv import escape_filter_chars
 import re
 
-from jupyterhub.auth import Authenticator
+from jupyterhub.auth import Authenticator, LocalAuthenticator
 from tornado import gen
 from traitlets import Unicode, Int, Bool, List, Union
 
@@ -333,3 +333,7 @@ class LDAPAuthenticator(Authenticator):
                 username=username,
             ))
             return None
+
+            
+class LDAPLocalAuthenticator(LocalAuthenticator, LDAPAuthenticator):
+    pass
